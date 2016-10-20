880d24c81c9900568bbad7574f0b06c4ea6a9027
Jason <jadomican@gmail.com>
2016-10-20 22:11:14 +0100

    Added main.cpp

---
+++ main.cpp
@@ -0,0 +1,26 @@
+/*
+Jason Domican		X00119321
+Robert Fitzgerald	X00123156
+
+ADS1 Practical 1
+REF: https://google.github.io/styleguide/cppguide.html
+*/
+
+#include <cstdlib>
+#include <fstream>
+#include <Windows.h>
+#include "similarity_index.h"
+using namespace std;
+
+int main()
+{
+	cout << "**Plagiarism Detector**\n";
+
+
+
+	system("pause");
+	return 0;
+}
+
+
+
