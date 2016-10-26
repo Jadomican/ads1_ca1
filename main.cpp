169bce732878c2d6141d2de02873014adf465c51
Jason Domican <jadomican@gmail.com>
2016-10-26 01:58:44 +0100

    Added basic "dirent.h" functionality (testing)

--- main.cpp
+++ main.cpp
@@ -9,11 +9,44 @@ ADS1 Practical 1
 #include <string>
 #include <fstream>
 #include <Windows.h>
+#include "dirent.h"
+
 #include "similarity_index.h"
 using namespace std;
 
 int main()
 {
+
+	DIR *pdir = NULL; // struct in dirent.h (Line 256)
+	struct dirent *pent = NULL;
+
+	//char *directory = NULL;
+	//directory = "./files";
+
+	pdir = opendir("./files"); //files folder INSIDE current directory
+
+	if (pdir == NULL)
+	{
+		cout << "pdir not initialised correctly";
+		exit(1);
+	}
+
+	cout << "LISTING DIRECTORY CONTENTS\n";
+	while (pent = readdir(pdir)) //while something left to list
+	{
+		if (pent == NULL)
+		{
+			cout << "pent not initialised correctly";
+			exit(1);
+		}
+
+		
+		cout << pent->d_name << endl;
+	}
+
+	closedir(pdir);
+
+	/*
 	string directory;
 
 	cout << "**Plagiarism Detector**\n";
@@ -27,7 +60,7 @@ int main()
 	ofstream outfile;
 	outfile.open("files/file1.cpp", ios::app);
 	outfile.close();
-
+	*/
 
 	system("pause");
 	return 0;
