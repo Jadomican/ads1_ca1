3422ac20a52b814796967e1239d05e519e6229f0
Jason Domican <jadomican@gmail.com>
2016-10-30 22:05:46 +0000

    Added A LOT
    Can now read any file name, also can pass in directory from user

--- main.cpp
+++ main.cpp
@@ -11,72 +11,70 @@ ADS1 Practical 1
 #include <Windows.h>
 #include "dirent.h"
 #include <sstream>
+#include <vector>
+
 
 #include "similarity_index.h"
 using namespace std;
 
 int main()
 {
-	/*
+	const int NUM_FILES = 4;	//number of files to be processed
+	SimilarityIndex array_of_indexes[NUM_FILES]; //	NOT YET USED
+
+	//string file_names_strings[NUM_FILES];
+
+	vector<string> file_names = vector<string>();
+
 	DIR *pdir = NULL; // struct in dirent.h (Line 256)
 	struct dirent *pent = NULL;
-
-	//char *directory = NULL;
-	//directory = "./files";
 
 	pdir = opendir("./files"); //files folder INSIDE current directory
 
 	if (pdir == NULL)
 	{
-	cout << "pdir not initialised correctly";
-	exit(1);
+		cout << "pdir not initialised correctly";
+		exit(1);
 	}
 
+	int index = 0;
 	cout << "LISTING DIRECTORY CONTENTS\n";
 	while (pent = readdir(pdir)) //while something left to list
 	{
-	if (pent == NULL)
-	{
-	cout << "pent not initialised correctly";
-	exit(1);
+		if (pent == NULL)
+		{
+			cout << "pent not initialised correctly";
+			exit(1);
+		}
+
+		file_names.push_back(string(pent->d_name));
 	}
 
-	cout << pent->d_name << endl;
+	for (int i = 0; i < NUM_FILES; i++)
+	{
+		cout << file_names[i + 2] << endl;
+		//cout << file_names_strings[i] << endl;
 	}
 
 	closedir(pdir);
 
-	*/
-
-	//string directory;
-
+	string directory;
 	cout << "**Plagiarism Detector**";
-	//cout << "Enter the directory to be scanned: ";
-	//cin >> directory;
+	cout << "Input directory to be scanned: (HINT: Try ./files/)" << endl;
+	cin >> directory;
 
-
-	//USES windows.h RATHER THAN DIRENT!!
-
-	const int NUM_FILES = 4;	//number of files to be processed
-
-	SimilarityIndex array_of_indexes[NUM_FILES]; //	NOT YET USED
-
-/***************************************************************************************
-*    Usage: based on
-*    Title: How do you make an array of structs in C?
-*    Date:  27/10/2016
-*    Availability: http://stackoverflow.com/questions/10468128/how-do-you-make-an-array-of-structs-in-c
-***************************************************************************************/
-
+	/***************************************************************************************
+	*    Usage: based on
+	*    Title: How do you make an array of structs in C?
+	*    Date:  27/10/2016
+	*    Availability: http://stackoverflow.com/questions/10468128/how-do-you-make-an-array-of-structs-in-c
+	***************************************************************************************/
 
 	for (int i = 0; i < NUM_FILES; i++)
 	{
-		array_of_indexes[i].count_iterative = 0;
-		array_of_indexes[i].count_selective = 0; //	USE TO COUNT MATCHES OF ITERATIVE AND SELECTIVE
-
 		string line;
 		stringstream file_path;
-		file_path << "./files/file" << i + 1 << ".cpp";
+		file_path << directory << file_names[i + 2];
 
 		ifstream inFile;		//reading in
 		inFile.open(file_path.str()); //pass in file_path as string
@@ -87,7 +85,6 @@ int main()
 			inFile >> line;
 
 			//check if contains selective/iterative
-
 			if (line == "int")
 			{
 				array_of_indexes[i].count_iterative++;
@@ -99,9 +96,7 @@ int main()
 
 		inFile.close(); //remember to close file
 
-		cout << "FILE CONTAINS: " << array_of_indexes[i].count_iterative << " ITERATIVE STATEMENTS\n\n";
-
-
+		cout << "\nFILE CONTAINS: " << array_of_indexes[i].count_iterative << " ITERATIVE STATEMENTS\n\n";
 
 	}
 
