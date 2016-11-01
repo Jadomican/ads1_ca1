1dd03a9264d5e4fec811f6371f0b53e8ae8158a4
Jason Domican <jadomican@gmail.com>
2016-11-01 15:53:30 +0000

    added more test files

---
+++ files/school.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	for
+	for
+	while
+	if
+	switch
+	int count = 1;
+	while (count <= 5)
+	{
+		int count = 1;
+		cout << count << "\n";
+		count++;
+	}
+	system("pause");
+	return 0;
+}
+//should have 4 iterative statements
+//should have 2 selection statements
---
+++ files/zfile6.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	for
+	for
+	while
+	if
+
+	int count = 1;
+	while (count <= 5)
+	{
+		int count = 1;
+		cout << count << "\n";
+		count++;
+	}
+
+	system("pause");
+	return 0;
+}
+
+//should have 4 iterative statements
+//should have 1 selection statements
