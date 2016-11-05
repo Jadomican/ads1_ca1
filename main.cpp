f6a936af515ec253acb9c9d879c083c0bbbc8ae9
Jason Domican <jadomican@gmail.com>
2016-11-05 00:17:04 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,25 +13,34 @@ ADS1 Practical 1
 #include <sstream>
 #include <vector>
 #include "dirent.h"
+#include <iomanip>
+//#include <algorithm> //can potentially use to SORT?
 using namespace std;
 
-int main()
+int main() //all parts of program coded by Jason and Robert
 {
 	struct SimilarityIndex
 	{
-		int count_selective = 0; // if, switch
-		int count_iterative = 0; // for, while
-		string file_name;
+		int count_selective = 0; // count of selective statements
+		int count_iterative = 0; // count of iterative statements
 		int number_of_lines = 0; //lines of CODE
-
+		int number_of_words = 0; //word count
+		string file_name;
+		double index_metric; //how you determine similarity
 	};
 
-	//https://google.github.io/styleguide/cppguide.html#Constant_Names
-	const int kMaxNumFiles = 4;						//number of files to be processed
-	SimilarityIndex array_of_indexes[kMaxNumFiles];	//SHOULD WE INCLUDE A STRING (file_name) VARIABLE INSIDE THE STRUCT?
+	/***************************************************************************************
+	*    Usage: modified
+	*    Title: Accessing directories in C/C++ Part I
+	*    Date: 29/10/2016
+	*    Availability: http://www.dreamincode.net/forums/topic/59943-accessing-directories-in-cc-part-i/
+	***************************************************************************************/
+
+	const int kMaxNumFiles = 4;						//MAX number of files to be processed
+	SimilarityIndex array_of_indexes[kMaxNumFiles];
 	string directory;
-	DIR *pdir = NULL;								// struct in dirent.h (Line 256)
-	struct dirent *pent = NULL;
+	DIR *pdir = NULL;			// pointer to a directory
+	struct dirent *pent = NULL; //dirent pointer
 	int count_files = 0;
 
 	do
@@ -89,7 +98,7 @@ int main()
 	//}
 
 	int num_skipped = 0;
-	while (pent = readdir(pdir)) //while something left to list ANY NUMBER OF FILES
+	while (pent = readdir(pdir)) //while something left to list - read next directory entry
 	{
 
 		if (pent == NULL)
@@ -124,12 +133,12 @@ int main()
 
 	closedir(pdir); //close the directory when done reading
 
-	/***************************************************************************************
-	*    Usage: based on
-	*    Title: How do you make an array of structs in C?
-	*    Date:  27/10/2016
-	*    Availability: http://stackoverflow.com/questions/10468128/how-do-you-make-an-array-of-structs-in-c
-	***************************************************************************************/
+					/***************************************************************************************
+					*    Usage: based on
+					*    Title: How do you make an array of structs in C?
+					*    Date:  27/10/2016
+					*    Availability: http://stackoverflow.com/questions/10468128/how-do-you-make-an-array-of-structs-in-c
+					***************************************************************************************/
 
 	for (int i = 0; i < count_files; i++)
 	{
@@ -139,16 +148,26 @@ int main()
 		file_path << directory << array_of_indexes[i].file_name; //ignore . AND .. in file listing
 
 		string line;
-		ifstream inFile;		//reading in
+		ifstream wc_in_file;		//READ NUMBER OF WORDS
+		ifstream in_file;			//READ NUMBER OF SEL/ITER/NUMBER OF LINES
 
-		inFile.open(file_path.str()); //READ NUMBER OF LINES
-		while (getline(inFile, line))
+		wc_in_file.open(file_path.str());
+		while (getline(wc_in_file, line))
 		{
 			array_of_indexes[i].number_of_lines++; //NUMBER OF LINES WITH CODE IN THEM
 
-			inFile >> string_in;
+			wc_in_file >> string_in;
 
-			//check if contains selective/iterative
+		}
+
+		wc_in_file.close();
+
+		in_file.open(file_path.str());	//pass in file_path as string
+		while (!in_file.eof()) {			//READ EACH STRING
+
+			in_file >> string_in;
+			array_of_indexes[i].number_of_words++;
+			////check if contains selective/iterative
 			if (string_in == "for" || string_in == "while" || string_in == "do while")
 			{
 				array_of_indexes[i].count_iterative++;
@@ -160,32 +179,26 @@ int main()
 			}
 
 		}
-		inFile.close();
+		in_file.close(); //remember to close file
 
-		//inFile.open(file_path.str());	//pass in file_path as string
-		//while (!inFile.eof()) {			//READ EACH STRING
+		array_of_indexes[i].index_metric =
+			(double(array_of_indexes[i].count_iterative) + double(array_of_indexes[i].count_selective))
+			* double(array_of_indexes[i].number_of_words) / double(array_of_indexes[i].number_of_lines);
 
-		//	inFile >> string_in;
-
-		//	//check if contains selective/iterative
-		//	if (string_in == "for" || string_in == "while" || string_in == "do while")
-		//	{
-		//		array_of_indexes[i].count_iterative++;
-		//	}
-
-		//	if (string_in == "if" || string_in == "switch" || string_in == "case")
-		//	{
-		//		array_of_indexes[i].count_selective++;
-		//	}
-
-		//}
-		//inFile.close(); //remember to close file
-
+		/***************************************************************************************
+		*    Usage: modified
+		*    Title: Show two digits after decimal point in c++
+		*    Date: 04/11/2016
+		*    Availability: http://stackoverflow.com/questions/16280069/show-two-digits-after-decimal-point-in-c
+		***************************************************************************************/
 		cout << endl << array_of_indexes[i].file_name;
 		cout << "\n   FILE CONTAINS: " << array_of_indexes[i].count_iterative << " ITERATIVE STATEMENTS\n";
 		cout << "   FILE CONTAINS: " << array_of_indexes[i].count_selective << " SELECTIVE STATEMENTS\n";
 		cout << "   Number of lines (of code) in text file: " << array_of_indexes[i].number_of_lines << endl;
+		cout << "   Word count: " << array_of_indexes[i].number_of_words << endl;
 
+		//format print statement to 2 decimal places for readability
+		cout << setprecision(2) << fixed << "   INDEX METRIC: " << array_of_indexes[i].index_metric << endl;
 
 	}
 
@@ -194,11 +207,6 @@ int main()
 	return 0;
 }
 
-
-//USE VECTOR, ARRAY OF STRINGS OR STRING VARIABLE IN similarity_index.h?
-//HOW MANY COMPARISONS? WHILE? DO? FOR? FOREACH? RETURN? WHICH WAY? find() method?
-
 //http://www.cplusplus.com/reference/string/string/back/
 //https://www.daniweb.com/programming/software-development/threads/369727/open-txt-files-one-by-one-from-directory
 //http://stackoverflow.com/questions/2340281/check-if-a-string-contains-a-string-in-c
-//http://www.dreamincode.net/forums/topic/59943-accessing-directories-in-cc-part-i/
