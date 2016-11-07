b39df6978bd61c4b7505e0374e8d1410be3ce139
Jason Domican <jadomican@gmail.com>
2016-11-07 01:22:38 +0000

    ABILITY TO LOOP AROUND AGAIN (NOT NEEDED)

--- main.cpp
+++ main.cpp
@@ -1,208 +1,270 @@
 /*
-Jason Domican		X00119321
-Robert Fitzgerald	X00123156
-
-ADS1 Practical 1
+*	Jason Domican		X00119321
+*	Robert Fitzgerald	X00123156
+*
+*	ADS1 CA1
+*	Utilises dirent.h by Toni Rönkkö https://github.com/tronkko
 */
 
 #include <cstdlib>
 #include <iostream>
 #include <string>
 #include <fstream>
-#include <Windows.h>
 #include <sstream>
 #include <vector>
-#include "dirent.h"
-
-//#include <algorithm>
-
+#include <iomanip>
+#include <algorithm>
+#include "dirent.h" //used to access directories
 using namespace std;
 
-int main()
+int main() //all parts of program coded by Jason and Robert
 {
 	struct SimilarityIndex
 	{
-		int count_selective = 0; // if, switch
-		int count_iterative = 0; // for, while
+		int count_selective = 0; // count of selective statements
+		int count_iterative = 0; // count of iterative statements
 		int number_of_lines = 0; //lines of CODE
-		int number_of_words = 0;
-
-		double index_metric; //= count_selective + count_iterative; //how you determine similarity
-
+		int number_of_words = 0; //word count
 		string file_name;
+		double index_metric; //determines similarity
 	};
 
-	//https://google.github.io/styleguide/cppguide.html#Constant_Names
-	const int kMaxNumFiles = 4;						//number of files to be processed
-	SimilarityIndex array_of_indexes[kMaxNumFiles];	//SHOULD WE INCLUDE A STRING (file_name) VARIABLE INSIDE THE STRUCT?
+	/***************************************************************************************
+	*    Usage: modified
+	*    Title: Accessing directories in C/C++ Part I
+	*    Date: 29/10/2016
+	*    Availability: http://www.dreamincode.net/forums/topic/59943-accessing-directories-in-cc-part-i/
+	*
+	*    Usage: modified
+	*    Title: Open txt files one by one from directory
+	*    Date: 29/10/2016
+	*    Availability: https://www.daniweb.com/programming/software-development/threads/369727/open-txt-files-one-by-one-from-directory
+	***************************************************************************************/
+
+	const int kMaxNumFiles = 4; //MAX number of files to be processed
+	SimilarityIndex array_of_indexes[kMaxNumFiles];
 	string directory;
-	DIR *pdir = NULL;								// struct in dirent.h (Line 256)
-	struct dirent *pent = NULL;
-	int count_files = 0;
+	DIR *pdir = NULL; // pointer to a directory
+	struct dirent *pent = NULL; //dirent pointer
+	int count_files;
+	int num_skipped; //number of files in directory skipped based on max files constant
+	double plagiarism_percent = 0.30; //percentage of similarity checked
+	bool continue_run = true;
+	char choice;
+	cout << "**Plagiarism Detector (Max " << kMaxNumFiles << " .cpp files)**\n";
 
-	do
+	while (continue_run)
 	{
-		cout << "**Plagiarism Detector (MAX " << kMaxNumFiles << " FILES)**";
-		cout << "\nInput directory to be scanned: (HINT: Try ./files/ or files/ ... x to exit)\n\nDirectory: ";
-		cin >> directory;
-
-		if (directory == "x") // allow user to exit
+		cout << "Input directory to be scanned or press x to exit)\n\n";
+		count_files = 0;
+		num_skipped = 0;
+		do
 		{
-			cout << "\nExiting...\n";
-			system("pause");
-			exit(1);
-		}
+			cout << "Directory: ";
+			cin >> directory;
 
-		if (directory.back() != '/') //last character of string
-		{
-			directory += '/';
-		}
+			if (directory == "x" || directory == "X") //allow user to exit
+			{
+				cout << "\nExiting...\n";
+				system("pause");
+				exit(1);
+			}
+
+			/***************************************************************************************
+			*    Usage: modified
+			*    Title: String back() method
+			*    Date: 30/10/2016
+			*    Availability: http://www.cplusplus.com/reference/string/string/back/
+			***************************************************************************************/
+
+			if (directory.back() != '/') //ensure last character is a '/' ('\' works too)
+			{
+				directory += '/';
+			}
+
+			/***************************************************************************************
+			*    Usage: modified
+			*    Title: Converting String to Cstring in c++
+			*    Date: 30/10/2016
+			*    Availability: http://stackoverflow.com/questions/11821491/converting-string-to-cstring-in-c
+			***************************************************************************************/
+
+			pdir = opendir(directory.c_str()); //opens specified directory
+
+			if (pdir == NULL)
+			{
+				cout << "\nDirectory not found, try again\n\n";
+			}
+		} while (pdir == NULL);
 
 		/***************************************************************************************
 		*    Usage: modified
-		*    Title: Converting String to Cstring in c++
-		*    Date: 30/10/2016
-		*    Availability: http://stackoverflow.com/questions/11821491/converting-string-to-cstring-in-c
+		*    Title: How to get file extension from string in C++
+		*    Date: 03/11/2016
+		*    Availability: http://stackoverflow.com/questions/51949/how-to-get-file-extension-from-string-in-c
 		***************************************************************************************/
 
-		pdir = opendir(directory.c_str()); //opens specified directory
-
-		if (pdir == NULL)
+		while (pent = readdir(pdir)) //while something left to list - read next directory entry
 		{
-			cout << "\npdir not initialised correctly\n";
-			cout << "\nTRY AGAIN\n";
-		}
-	} while (pdir == NULL);
-
-	//FOR LOOP OR WHILE LOOP?????
-	//for (int i = 0; i < NUM_FILES + 2; i++) //IGNORE . AND .. (max 4 files)
-	//{
-	//	pent = readdir(pdir);
-	//	if (pent == NULL)
-	//	{
-	//		cout << "pent not initialised correctly";
-	//		exit(1);
-	//	}
-	//
-	//	if (string(pent->d_name) == "." || string(pent->d_name) == "..") //do not include root and parent directory
-	//	{
-	//		//do nothing
-	//	}
-	//	else
-	//	{
-	//		array_of_indexes[i].file_name = string(pent->d_name);
-	//	}
-	//}
-
-	int num_skipped = 0;
-	while (pent = readdir(pdir)) //while something left to list ANY NUMBER OF FILES
-	{
-
-		if (pent == NULL)
-		{
-			cout << "pent not initialised correctly";
-			exit(1);
-		}
-
-		if (string(pent->d_name) == "." || string(pent->d_name) == "..") //do not include current and parent directory
-		{
-			//do nothing
-		}
-		else
-		{
-			if (count_files >= kMaxNumFiles)
+			if (string(pent->d_name).substr(string(pent->d_name).find_last_of(".") + 1) == "cpp") //if cpp file type
 			{
-				num_skipped++;
+				if (count_files >= kMaxNumFiles)
+				{
+					num_skipped++;
+				}
+				else
+				{
+					array_of_indexes[count_files].file_name = string(pent->d_name);
+					count_files++;
+				}
+			}
+		}
+
+		if (count_files < 1) //if no cpp files found
+		{
+			cout << "\n*No .cpp files found*\n";
+		}
+		else //else continue
+		{
+			if (num_skipped > 0)
+			{
+				cout << "\n*" << num_skipped << " .cpp files were skipped*\n";
+			}
+
+			closedir(pdir); //close the directory when done reading
+
+			/***************************************************************************************
+			*    Usage: based on
+			*    Title: How do you make an array of structs in C?
+			*    Date:  27/10/2016
+			*    Availability: http://stackoverflow.com/questions/10468128/how-do-you-make-an-array-of-structs-in-c
+			***************************************************************************************/
+
+			for (int i = 0; i < count_files; i++)
+			{
+				array_of_indexes[i].count_selective = 0; //reset values (for if user decides to run again)
+				array_of_indexes[i].count_iterative = 0;
+				array_of_indexes[i].number_of_lines = 0;
+				array_of_indexes[i].number_of_words = 0;
+				array_of_indexes[i].index_metric = 0;
+
+				string string_in; //reads only a SINGLE string in at a time
+				stringstream file_path;
+				file_path << directory << array_of_indexes[i].file_name; //ignore . AND .. in file listing
+
+				string line;
+				ifstream lines_in_file;		//READ NUMBER OF LINES
+				ifstream in_file;			//READ NUMBER OF WORDS/SEL/ITER/
+
+				lines_in_file.open(file_path.str());
+				while (getline(lines_in_file, line))
+				{
+					array_of_indexes[i].number_of_lines++; //number of lines of code
+					lines_in_file >> string_in;
+				}
+				lines_in_file.close();
+
+				in_file.open(file_path.str());	//pass in file_path as string
+				while (!in_file.eof()) {		//Read each string
+
+					in_file >> string_in;
+					array_of_indexes[i].number_of_words++;
+					//check if contains selective/iterative
+					if (string_in == "for" || string_in == "while" || string_in == "do while")
+					{
+						array_of_indexes[i].count_iterative++;
+					}
+
+					if (string_in == "if" || string_in == "switch" || string_in == "case")
+					{
+						array_of_indexes[i].count_selective++;
+					}
+				}
+				in_file.close(); //remember to close file
+
+				//calculate similarity index metric
+				array_of_indexes[i].index_metric =
+					((array_of_indexes[i].count_iterative) + (array_of_indexes[i].count_selective))
+					* double(array_of_indexes[i].number_of_words) / (array_of_indexes[i].number_of_lines);
+			}
+
+			/***************************************************************************************
+			*    Usage: modified
+			*    Title: Sorting an array using selection sort
+			*    Date:  1/11/2016
+			*    Availability: http://www.learncpp.com/cpp-tutorial/64-sorting-an-array-using-selection-sort/
+			***************************************************************************************/
+
+			//Sort by index metric
+			cout << "\nSorting by index metrics:\n";
+			for (int start_index = 0; start_index < count_files; start_index++)
+			{
+				// smallest_index is the index of the smallest element encountered so far.
+				int smallest_index = start_index;
+				// Look for smallest element remaining in the array (starting at start_index+1)
+				for (int current_index = start_index + 1; current_index < count_files; current_index++)
+				{
+					// If the current element is smaller than the previously found smallest
+					if (array_of_indexes[current_index].index_metric < array_of_indexes[smallest_index].index_metric)
+						// This is the new smallest number for this iteration
+						smallest_index = current_index;
+				}
+				// Swap our start element with our smallest element
+				swap(array_of_indexes[start_index], array_of_indexes[smallest_index]);
+			}
+
+			/***************************************************************************************
+			*    Usage: modified
+			*    Title: Show two digits after decimal point in c++
+			*    Date: 04/11/2016
+			*    Availability: http://stackoverflow.com/questions/16280069/show-two-digits-after-decimal-point-in-c
+			***************************************************************************************/
+
+			for (int i = 0; i < count_files; i++)
+			{
+				cout << "\n" << array_of_indexes[i].file_name;
+				cout << "\n   File contains: " << array_of_indexes[i].count_iterative << " Iterative statements\n";
+				cout << "   File contains: " << array_of_indexes[i].count_selective << " Selective statements\n";
+				cout << "   Number of lines (of code) in text file: " << array_of_indexes[i].number_of_lines << "\n";
+				cout << "   Word count: " << array_of_indexes[i].number_of_words << "\n";
+				//format print statement to 2 decimal places for readability
+				cout << setprecision(2) << fixed << "   Index Metric: " << array_of_indexes[i].index_metric << "\n";
+			}
+
+			if (count_files > 1)
+			{
+				for (int i = 0; i < count_files; i++) //determine similarity
+				{
+					for (int j = i + 1; j < count_files + 1; j++)
+					{
+						if (array_of_indexes[i].index_metric < (array_of_indexes[j].index_metric + array_of_indexes[j].index_metric * plagiarism_percent)
+							&& (array_of_indexes[i].index_metric > (array_of_indexes[j].index_metric - array_of_indexes[j].index_metric * plagiarism_percent)))
+						{
+							cout << "\n" << array_of_indexes[i].file_name << " and " << array_of_indexes[j].file_name
+								<< " may be plagiarised (within " << plagiarism_percent * 100 << "%)" << "\n";
+						}
+					}
+				}
 			}
 			else
 			{
-				//file_names.push_back(string(pent->d_name));
-				array_of_indexes[count_files].file_name = string(pent->d_name);
-				count_files++;
+				cout << "\n*Only one file found, similarity cannot be determined*";
 			}
 		}
-	}
 
-	if (num_skipped > 0)
-	{
-		cout << "\n*" << num_skipped << " files were skipped*\n";
-	}
-
-	closedir(pdir); //close the directory when done reading
-
-					/***************************************************************************************
-					*    Usage: based on
-					*    Title: How do you make an array of structs in C?
-					*    Date:  27/10/2016
-					*    Availability: http://stackoverflow.com/questions/10468128/how-do-you-make-an-array-of-structs-in-c
-					***************************************************************************************/
-
-	for (int i = 0; i < count_files; i++)
-	{
-		string string_in; //reads only a SINGLE string in at a time
-		stringstream file_path;
-		//file_path << directory << file_names[i]; //ignore . AND .. in file listing
-		file_path << directory << array_of_indexes[i].file_name; //ignore . AND .. in file listing
-
-		string line;
-		ifstream wc_in_file;		//READ NUMBER OF WORDS
-		ifstream in_file;			//READ NUMBER OF SEL/ITER/NUMBER OF LINES
-
-		wc_in_file.open(file_path.str());
-		while (getline(wc_in_file, line))
+		cout << "\nWould you like to go again? Y to continue...\nChoice: ";
+		cin >> choice;
+		
+		if (choice == 'Y' || choice == 'y')
 		{
-			array_of_indexes[i].number_of_lines++; //NUMBER OF LINES WITH CODE IN THEM
-
-			wc_in_file >> string_in;
-
-			//check if contains selective/iterative
-			//if (string_in == "for" || string_in == "while" || string_in == "do while")
-			//{
-			//	array_of_indexes[i].count_iterative++;
-			//}
-
-			//if (string_in == "if" || string_in == "switch" || string_in == "case")
-			//{
-			//	array_of_indexes[i].count_selective++;
-			//}
+			continue_run = true;
+			system("cls");
 
 		}
-
-
-		wc_in_file.close();
-
-		in_file.open(file_path.str());	//pass in file_path as string
-		while (!in_file.eof()) {			//READ EACH STRING
-
-			in_file >> string_in;
-			array_of_indexes[i].number_of_words++;
-			////check if contains selective/iterative
-			if (string_in == "for" || string_in == "while" || string_in == "do while")
-			{
-				array_of_indexes[i].count_iterative++;
-			}
-
-			if (string_in == "if" || string_in == "switch" || string_in == "case")
-			{
-				array_of_indexes[i].count_selective++;
-			}
-
+		else
+		{
+			continue_run = false;
 		}
-		in_file.close(); //remember to close file
-
-
-		array_of_indexes[i].index_metric = 
-			(double(array_of_indexes[i].count_iterative) + double(array_of_indexes[i].count_selective)) 
-			* double(array_of_indexes[i].number_of_words) / double(array_of_indexes[i].number_of_words);
-
-
-		cout << endl << array_of_indexes[i].file_name;
-		cout << "\n   FILE CONTAINS: " << array_of_indexes[i].count_iterative << " ITERATIVE STATEMENTS\n";
-		cout << "   FILE CONTAINS: " << array_of_indexes[i].count_selective << " SELECTIVE STATEMENTS\n";
-		cout << "   Number of lines (of code) in text file: " << array_of_indexes[i].number_of_lines << endl;
-		cout << "   Word count: " << array_of_indexes[i].number_of_words << endl;
-		cout << "   INDEX METRIC: " << array_of_indexes[i].index_metric << endl;
-
 
 	}
 
@@ -210,8 +272,3 @@ int main()
 	system("pause");
 	return 0;
 }
-
-//http://www.cplusplus.com/reference/string/string/back/
-//https://www.daniweb.com/programming/software-development/threads/369727/open-txt-files-one-by-one-from-directory
-//http://stackoverflow.com/questions/2340281/check-if-a-string-contains-a-string-in-c
-//http://www.dreamincode.net/forums/topic/59943-accessing-directories-in-cc-part-i/
